386f6918d1725d84bd0262040cb2e3dba77d0bd9
mayke0 <mayke2_0@yahoo.com>
2024-01-10 16:35:37 -0300

    atualização

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,4 +30,5 @@ function updateSoftSkills(profileData){
 (async () =>{
     const profileData = await fetchProfileData()
     updateProfileInfo(profileData)
+    updateSoftSkills(profileData)
 })()
